45b0e315447e78eaa41fe4e66b00802ef9ab7d60
Stakhiev Alexander <sashastakhiev@gmail.com>
2021-02-24 14:53:37 +0000

    fix

--- utils/flow_utils.py
+++ utils/flow_utils.py
@@ -69,7 +69,7 @@ def visulize_flow_file(flow_filename, save_dir=None):
     # plt.imshow(img)
     # plt.show()
     if save_dir:
-        idx = flow_filename.rfind("\\") + 1
+        idx = flow_filename.rfind("/") + 1
         plt.imsave(os.path.join(save_dir, "%s-vis.png" % flow_filename[idx:-4]), img)
 
 
